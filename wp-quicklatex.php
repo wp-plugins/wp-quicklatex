b5d7617ba174dac5421098560e46abbf4840cf81
advanpix <advanpix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-22 07:40:01 +0000

    = 2.4 = * Increase speed by using cURL if allowed by the server configuration. * Support of restricted servers with disabled `allow_url_fopen`.
    git-svn-id: https://plugins.svn.wordpress.org/wp-quicklatex/trunk@128371 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-quicklatex.php
+++ wp-quicklatex.php
@@ -3,26 +3,35 @@
 	Plugin Name: WP QuickLaTeX
 	Plugin URI: http://www.holoborodko.com/pavel/?page_id=1422
 	Description: Allows user to insert mathematical formulas in the posts and comments using LaTeX language.				 Usual LaTeX plugins suffer from incorrect formula positioning relative to surrounding text producing "jumpy" equations painful for eyes and decreasing overall readability of the web article. WP QuickLaTeX is the only one plugin which solves this issue. Just wrap LaTeX code with [tex][/tex] or [latex][/latex] or [math][/math] tags. WP QuickLaTeX will convert it to high-quality image and embed into post with proper positioning so that formula and surrounding text will blend together well.
-	Version: 2.3
+	Version: 2.4
 	Author: Pavel Holoborodko
 	Author URI: http://www.holoborodko.com/pavel/
 	Copyright: Pavel Holoborodko
 	License: GPL2
 */
 
-// CURL Extension Emulation Library by Steve Blinch
-// http://code.blitzaffe.com
-// require_once('libcurlemu.inc.php');
-
 if ( !defined('ABSPATH') ) exit;
 
 class WP_QuickLatex{
 	var $options;
+	var $fetch_remote_type; // 0 - none, 1 - cURL, 2 - file_get_contents, 3 - fsockopen
+	var $fetch_errstr;
+	var $fetch_errno;
 	
 	function init() {
 			//1. ToDo: Load Options if Any
 
-			//2. Add filters wisely (or register shortcode handlers)
+			//2. Detect fetch_remote_type
+			$this->fetch_remote_type = 0;
+			if(function_exists('curl_init')){ 
+				$this->fetch_remote_type = 1;
+			}elseif (ini_get('allow_url_fopen') == '1' && function_exists('file_get_contents')){
+				$this->fetch_remote_type = 2;
+			}elseif(function_exists('fsockopen')){
+				$this->fetch_remote_type = 3;			
+			}
+
+			//3. Add filters wisely (or register shortcode handlers)
 			add_filter('the_content', array(&$this, 'ql_convert'));
 			add_filter('comment_text', array(&$this, 'ql_convert'));			
 			add_filter('the_title', array(&$this, 'ql_convert'));
@@ -42,6 +51,8 @@ class WP_QuickLatex{
 		// Caching begin
 		$formula_hash = md5($formula_text);
 
+		// The script will automatically create a folder called backup-db in wp-content 
+		// folder if that folder is writable. If it is not created, please create it and CHMOD it to 777.
 		$cache_dir = 'wp-content/ql-cache';
 		$cache_path = ABSPATH.$cache_dir; 
 
@@ -72,8 +83,13 @@ class WP_QuickLatex{
 				
 				// Build URI to request server
 				// Don't forget to acknowledge QuickLaTeX.com on your page
-				$latex_server_url = "http://www.quicklatex.com/latex.f?formula=".rawurlencode($formula_text);		
-				$server_resp = file_get_contents($latex_server_url);						
+				//$latex_server_url = "http://www.quicklatex.com/latex.f?formula=".rawurlencode($formula_text);		
+				//$server_resp = file_get_contents($latex_server_url);						
+				$server_resp = $this->file_get_contents_flex('www.quicklatex.com','latex.f?formula='.rawurlencode($formula_text));											
+
+				if($server_resp==false){
+					return $this->fetch_errstr;
+				}
 
 				// Parse server's response
 				if (ereg("^([-]?[0-9]+)\r{1}\n{1}(.+)[ ]+([-]?[0-9]+)\r?\n?(.*)$", $server_resp, $regs)) 
@@ -167,6 +183,88 @@ class WP_QuickLatex{
 										. chr(0x80 | $c >> 6 & 0x3F)
 										. chr(0x80 | $c & 0x3F);
 		} else {
+			return false;
+		}
+	}
+	
+	//	Returns remote file contents if ok, false otrherwise 
+	function file_get_contents_flex($host, $query)
+	{
+		$url = "http://$host/$query";		
+
+		$this->fetch_errstr = '';
+		$this->fetch_errno = 0;
+
+		if($this->fetch_remote_type == 1 ){
+			//1. cURL
+			if($ch = curl_init()){
+				
+				curl_setopt($ch, CURLOPT_URL, $url);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);		
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
+				curl_setopt($ch, CURLOPT_HEADER, 0);
+				curl_setopt($ch, CURLOPT_CRLF, 0);
+				
+				$data = curl_exec($ch);
+				
+				if($data==false){
+					$state = curl_getinfo($ch);		
+					$this->fetch_errstr = 'curl error: '.curl_error($ch);
+					$this->fetch_errno = $state['http_code'];
+				}
+				
+				curl_close($ch);	
+				return $data;		
+			}else{
+				$this->fetch_errstr = 'curl error: curl_init() failed';
+				return false;
+			} 		
+			
+		}elseif($this->fetch_remote_type == 2){
+			
+			//2. file_get_contents			
+			if ($data = file_get_contents($url)){
+				return $data;
+			}else{
+				$this->fetch_errstr = 'file_get_contents failed';
+				return false;
+			}
+			
+		}elseif($this->fetch_remote_type == 3){
+			//3. fsockopen		
+			$fp = fsockopen($host, '80', $this->fetch_errno, $this->fetch_errstr, 10);
+			if (!$fp) {
+				$this->fetch_errstr  = 'fsockopen error: '.$this->fetch_errstr;
+				return false;
+			} else {
+				$out = "GET /$query HTTP/1.0\r\n";
+				$out .= "Host: $host\r\n";
+				$out .= "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.11) Gecko/2009060215 Firefox/3.0.11 GTB5 (.NET CLR 3.5.30729)\r\n";
+				$out .= "Accept: */*\r\n";		
+				$out .= "Accept-Language: en-us,en;q=0.5\r\n";
+				$out .= "Accept-Encoding: gzip,deflate\r\n";
+				$out .= "Accept-Charset: windows-1251,utf-8;q=0.7,*;q=0.7\r\n";
+				$out .= "Connection: keep-alive\r\n";
+				$out .= "Referer: http://$host\r\n";
+				$out .= "\r\n";
+				
+				fputs($fp, $out);
+
+				$buffer = '';
+
+				while(!feof($fp)) $buffer .= fgets($fp, 1024); 			
+				
+				fclose($fp);
+				
+				// strip the headers
+				$pos  = strpos($buffer, "\r\n\r\n");
+				$data = substr($buffer, $pos + 4);
+				
+				return $data;
+		   }				
+		}else{
+			$this->fetch_errstr  = 'Unknown fetch_remote_type';
 			return false;
 		}
 	}
